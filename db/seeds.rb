6fff473b21fb363de16097e55b01ba51d55e24dc
Elie Celka <elie.celka@gmail.com>
2024-11-27 18:16:36 +0100

    done

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,14 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+Restaurant.destroy_all
+
+puts "generating restaurants..."
+
+Restaurant.create!(name: "Epicure", category: "french", address: "75008 Paris")
+Restaurant.create!(name: "学中文", category: "chinese", address: "13001 Marseille")
+Restaurant.create!(name: "mamma mia", category: "italian", address: "38000 Grenoble")
+Restaurant.create!(name: "la baraque a frites", category: "belgian", address: "01300 Belley")
+Restaurant.create!(name: "寿司バー", category: "japanese", address: "69001 Lyon")
+
+puts "gererated #{Restaurant.count} restaurants"
